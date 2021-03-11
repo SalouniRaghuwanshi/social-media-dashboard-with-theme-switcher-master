82c290676cf85036452459ad82c1c496227077a2
salouni <salouniraghuwanshi@gmail.com>
2021-03-11 13:30:26 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-  
-var a=document.getElementsByTagName('body')[0];
-var b=document.getElementById("dark-mode");
-b.addEventListener('click',function(){
-  b.classList.toggle("on");
-  b.classList.toogle("of");
-})
+document.querySelector("input").addEventListener("click", function(){
+  $("body").toggleClass("dark-body");
+  $(".block").toggleClass("block-bg");
+  $(".block").toggleClass("block-bg-dark");
+  $(".facebook").toggleClass("dark-card");
+  $(".twitter").toggleClass("dark-card");
+  $(".instagram").toggleClass("dark-card");
+  $(".youtube").toggleClass("dark-card");
+  $(".overview-items").toggleClass("dark-card");
+  $(".facebook").toggleClass("background-white");
+  $(".twitter").toggleClass("background-white");
+  $(".instagram").toggleClass("background-white");
+  $(".youtube").toggleClass("background-white");
+  $(".overview-items").toggleClass("background-white");
+  $(".head1").toggleClass("dark-white");
+
+});
